46a45afb8d07e42e2871e5b2a41a830c60667b40
TheoWan19 <theowan24@gmail.com>
2023-09-26 04:02:05 -0700

    make it better

--- src/settings.py
+++ src/settings.py
@@ -61,7 +61,6 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

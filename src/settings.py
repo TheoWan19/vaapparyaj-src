83454497f0a81a6173ecd30780f9997f3c3ed74b
TheoWan19 <theowan24@gmail.com>
2023-09-26 05:22:41 -0700

    last commit

--- src/settings.py
+++ src/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'vaapparyaj-src-65ac6ecfe8f5.herokuapp.com', 'www.vaapparyaj.com', 'vaapparyaj.com']
 
 
 # Application definition
